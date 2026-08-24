3ed4e4d771e11be02d7b180d15a9c0894fa19dfb
Gyanti <gyantigupta2015@gmail.com>
2026-08-24 13:44:14 +0530

    first changes

---
+++ GitProjects/src/main/java/com/student/Student.java
@@ -0,0 +1,10 @@
+package com.student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Day 1- hello git");
+		System.out.println("Student Management System");
+	}
+
+}
